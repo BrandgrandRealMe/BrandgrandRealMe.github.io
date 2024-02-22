2811955010f5ba8b941a63b20f7eab1870167358
BrandgrandReal <47084010+BrandgrandRealMe@users.noreply.github.com>
2024-02-21 21:29:44 -0600

    Update HugoStart.bat

--- docs/how-to/DBH/quickstarts/HugoStart.bat
+++ docs/how-to/DBH/quickstarts/HugoStart.bat
@@ -1,13 +1,14 @@
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
   # Print an error message to standard error
-  echo "Usage: $0 port folder" >&2
+  echo "Usage: $0 port folder url" >&2
   # Exit with a non-zero status code
   exit 1
 fi
 
 port=$1
 folder=$2
+url=$3
 
 cd $folder
 
-../hugo server --bind 0.0.0.0 --port $port
+../hugo server --bind 0.0.0.0 --port $port --baseURL $url
